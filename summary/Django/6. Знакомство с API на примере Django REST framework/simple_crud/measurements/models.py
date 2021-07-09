5acb28a6bd66f051b580572e78a3240f81aebeaa
ervand7 <ervand7@mail.ru>
2021-07-09 21:34:07 +0300

    09.07.2021

--- summary/Django/6. Знакомство с API на примере Django REST framework/simple_crud/measurements/models.py
+++ summary/Django/6. Знакомство с API на примере Django REST framework/simple_crud/measurements/models.py
@@ -14,6 +14,13 @@ class Project(models.Model):
         auto_now=True
     )
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Объект на карте'
+        verbose_name_plural = 'Объекты на карте'
+
 
 class Measurement(models.Model):
     """Измерение температуры на объекте."""
@@ -21,8 +28,15 @@ class Measurement(models.Model):
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True  # автоматическое проставление даты при создании
     )
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True  # автоматическое проставление даты при обновлении
     )
+
+    def __str__(self):
+        return str(self.value)
+
+    class Meta:
+        verbose_name = 'Результат измерения'
+        verbose_name_plural = 'Результаты измерения'
--- summary/Django/6. Знакомство с API на примере Django REST framework/simple_crud/simple_crud/urls.py
+++ summary/Django/6. Знакомство с API на примере Django REST framework/simple_crud/simple_crud/urls.py
@@ -1,23 +1,17 @@
-"""simple_crud URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import debug_toolbar
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-# TODO: настройте роутер и подключите `ProjectViewSet` и `MeasurementViewSet`
+from measurements import api_views
+
+router = DefaultRouter()
+router.register('projects', api_views.ProjectViewSet, basename='projects')
+router.register('measurements', api_views.MeasurementViewSet, basename='measurements')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls), name='experiment'),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
